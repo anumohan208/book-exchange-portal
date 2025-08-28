b367a71fde6aafcc418e7781fbd09ecea451910b
anumohan208 <154403264+anumohan208@users.noreply.github.com>
2025-08-27 22:21:51 -0500

    Added post methods in backend for create book and get method for getting the book image

--- book-exchange-api/src/main/java/org/launchcode/book_exchange/Controllers/BookController.java
+++ book-exchange-api/src/main/java/org/launchcode/book_exchange/Controllers/BookController.java
@@ -3,11 +3,17 @@ package org.launchcode.book_exchange.Controllers;
 import org.launchcode.book_exchange.Models.Book;
 import org.launchcode.book_exchange.Repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -23,6 +29,49 @@ public class BookController {
     public List<Book> getAllEvents() {
 
         return bookRepository.findAll( );
+    }
+    @PostMapping
+    public ResponseEntity<Book> createBook(
+            @RequestParam("tittle") String tittle,
 
+            @RequestParam("author") String author,
+            @RequestParam("description") String description,
+            @RequestParam("conditionStatus") String conditionStatus,
+            @RequestParam("available") boolean available,
+            @RequestParam("owner") String owner,
+            @RequestParam("addedDate")@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate addedDate,
+            @RequestParam("bookImage") MultipartFile bookImage) {
+
+        Book book = new Book();
+        book.setTitle(tittle);
+        book.setAuthor(author);
+        book.setDescription(description);
+        book.setConditionStatus(conditionStatus);
+        book.setAvailable(available);
+        book.setOwner(owner);
+        book.setAddedDate(addedDate);
+
+
+        try {
+            book.setEventImage(bookImage.getBytes());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        Book savedEvent = bookRepository.save(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedEvent);
+    }
+    @GetMapping("/{id}/image")
+    public ResponseEntity<byte[]> getBookImage(@PathVariable Long id) {
+        Optional<Book> bookOptional = bookRepository.findById(id);
+        if (bookOptional.isPresent()) {
+            Book book = bookOptional.get();
+            byte[] imageBytes = book.getEventImage();
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(imageBytes);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
